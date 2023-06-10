783d8d15a8ef87d5a7b92173b5ff3382a2125cad
Yuje Lee <dbwp031@khu.ac.kr>
2023-06-10 15:50:59 +0900

    first commit

---
+++ src/main/java/com/example/springsecuritystudy3/SpringSecurityStudy3Application.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritystudy3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityStudy3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityStudy3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecuritystudy3/SpringSecurityStudy3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritystudy3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityStudy3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
